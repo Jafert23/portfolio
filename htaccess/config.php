2e50da2998a4df65fc3bfd76e0f6b5d9713e542f
Jafert23 <111777343+Jafert23@users.noreply.github.com>
2025-03-14 16:36:47 -0400

    file changes

--- htaccess/config.php
+++ htaccess/config.php
@@ -3,8 +3,8 @@
 
 // SMTP Configuration for PHPMailer
 define('SMTP_HOST', 'smtp.ionos.com'); // Your SMTP server
-define('SMTP_USERNAME', 'contact@creativeelliot.com'); // Your SMTP username
-define('SMTP_PASSWORD', 'ecjs2tGFA$XG!26'); // Your SMTP password
+define('SMTP_USERNAME', 'Enter Username'); // Your SMTP username
+define('SMTP_PASSWORD', 'Enter Password'); // Your SMTP password
 define('SMTP_PORT', 587); // SMTP port (e.g., 587 for TLS, 465 for SSL)
 define('SMTP_ENCRYPTION', 'tls'); // Encryption: 'tls' or 'ssl'
 
